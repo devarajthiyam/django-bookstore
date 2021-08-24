e579a912d147a66051f24224bbb86cc653c34d1f
T Devaraj Singh <devarajthiyam@gmail.com>
2021-08-25 00:13:47 +0530

    Datepicker widget input in Create form

--- book/forms.py
+++ book/forms.py
@@ -3,6 +3,10 @@ from .models import Book
 
 
 class BookForm(forms.ModelForm):
+    required_css_class = 'required-field'
+    publish_date = forms.DateField(widget=forms.TextInput(
+        attrs={'type' : 'date'}
+        ))
 
     class Meta:
         
@@ -11,3 +15,13 @@ class BookForm(forms.ModelForm):
         exclude = []
 
         labels = {}
+
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self:
+            field.field.widget.attrs.update({'class': 'form-control'})
+
+        self.fields['publish_date'].widget.attrs.update({'class': 'form-control'})
